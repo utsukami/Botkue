5ce976e74a38d06dbd28ae50e5419dc27a7b960c
utsukami <geremysowers@live.com>
2017-07-13 11:20:58 -0400

    minor fix for my minor fix

--- botkue/file_select.sh
+++ botkue/file_select.sh
@@ -1,7 +1,7 @@
-xdotool mousemove 2880 625 && sleep 1s && xdotool click 1 && sleep 1s;
+xdotool mousemove 2880 650 && sleep 1s && xdotool click 1 && sleep 1s;
 for x in $(xdotool search --name "Open File"); do 
 	xdotool mousemove --window $x 500 100; 
 	xdotool click 1; 
-	xdotool mousemove --window $x 900 475;
+	xdotool mousemove --window $x 900 510;
 	xdotool click 1;
  done
