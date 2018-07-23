ff2e55fa11f874ecc12b448a0887561bea1d2e46
utsukami <geremysowers@live.com>
2018-07-23 04:39:04 -0400

    Rewrote most of data_tracker attempting to follow PEP8

--- botkue/data_tracker.py
+++ botkue/data_tracker.py
@@ -1,92 +1,87 @@
-# Adding comments later
+import logging
+from datetime import datetime
+from os.path import expanduser
+from sqlite3 import connect
+from sys import argv
 
-import sys, sqlite3, logging, datetime
-from os.path import expanduser as euser
+home = expanduser('~')
+log_file = '{}/tmp/main.log'.format(home)
+format_date = datetime.utcnow().strftime("%Y-%m-%d")
 
-ranks = ('Smiley', 'Recruit', 'Corporal', 'Sergeant', 'Lieutenant', 'Captain', 'General')
-home = euser('~')
-
-conn = sqlite3.connect('{}/databases/main.sqlite'.format(home))
+conn = connect('{}/tmp/main.sqlite'.format(home))
 c = conn.cursor()
 
-fonn = sqlite3.connect('{}/databases/lost.sqlite'.format(home))
-f = fonn.cursor()
-
-arg_name = str(sys.argv[1])
-arg_rank = int(sys.argv[2])
-
-log_file = '{}/logs/main.log'.format(home)
-
-logging.basicConfig(filename=log_file,level=logging.INFO,
-        format='{}'.format(
-            '[%(asctime)s] %(message)s'),
-        datefmt='{}'.format(
-            '%Y-%m-%d %H:%M:%S')
+logging.basicConfig(
+    filename=log_file,
+    level=logging.INFO,
+    format='{}'.format('[%(asctime)s] %(message)s'),
+    datefmt='{}'.format('%Y-%m-%d %H:%M:%S')
 )
 
-current_time_utc = datetime.datetime.utcnow()
-format_time = current_time_utc.strftime("%Y-%m-%d")
+
+def rank_logger(name, rank, action):
+    ranks = (
+        "Smiley", "Recruit", "Corporal", "Sergeant",
+        "Lieutenant", "Captain", "General"
+    )
+
+    logging.info(
+        "ACTION: {} | NAME: {} | STATUS: {}"
+        .format(action, name, ranks[rank])
+    )
+
+
+def alter_ranks(name, rank, date, method):
+    if method == 1:
+        c.execute(
+            "UPDATE member SET "
+            "rank_id=?, last_seen=?, date_rank=? WHERE name=?",
+            (rank + 1, date, date, name,)
+        )
+
+    elif method == 2:
+        c.execute(
+            "UPDATE member SET last_seen=? WHERE name=?", (date, name,)
+        )
+
+    elif method == 3:
+        c.execute(
+            "INSERT OR IGNORE INTO member (name, rank_id, last_seen) "
+            "VALUES(?,?,?)", (name, rank + 1, date,)
+        )
+
+    conn.commit()
+
 
 def rank_tracker(name, rank_num):
-    c.execute("SELECT {} FROM member WHERE name='{}'".format(
-        'name, rank_id, last_seen', name))
-    
+    c.execute(
+        "SELECT name, rank_id FROM member WHERE name=?", (name,)
+    )
+
     try:
         data = c.fetchall()[0]
 
         if name in data[0]:
-            match_order = rank_num + 1
-            
-            if (match_order >= 1 
-                    and match_order <= 7):
-                
-                if (match_order < int(data[1]) 
-                        or match_order > int(data[1])):
-                    
-                    c.execute("UPDATE member SET {}'{}', {}'{}', {}'{}' WHERE {}'{}'".format(
-                        'rank_id=', rank_num + 1, 'last_seen=', format_time,
-                        'date_rank=', format_time, 'name=', data[0]))
-                    
-                    if (match_order < int(data[1]) 
-                            and match_order >= 1):
-                        
-                        logging.info('ACTION: {} | NAME: {} | STATUS: {}'.format(
-                            'Decrease', data[0], ranks[rank_num]))
-                    
-                    elif (match_order > int(data[1]) 
-                            and match_order <= 7):
-                        
-                        logging.info('ACTION: {} | NAME: {} | STATUS: {}'.format(
-                            'Increase', data[0], ranks[rank_num]))
-                   
-                    conn.commit()
-                
-                else:
-                    c.execute("UPDATE member SET {}'{}' WHERE {}'{}'".format(
-                        'last_seen=', format_time, 'name=', data[0]))
-                    conn.commit()
+            current_rank = int(data[1]) - 1
 
-            elif match_order == 0:
-                logging.info('ACTION: {} | NAME: {} | STATUS: {}'.format(
-                    'Removed', data[0], 'Inactive'))
-                
-                f.execute("INSERT INTO member ({}) VALUES('{}', '{}', '{}', '{}')".format(
-                    'name, rank_id, last_seen, reason', data[0],
-                    data[1], format_time, 'Removed'))
-               
-                c.execute("DELETE FROM member WHERE name='{}'".format(data[0]))
-                
-                fonn.commit()
-                conn.commit()
+            if 0 <= rank_num <= 6:
+                if rank_num < current_rank:
+                    rank_logger(data[0], rank_num, "Decrease")
+                    alter_ranks(data[0], rank_num, format_date, 1)
+
+                elif rank_num > current_rank:
+                    rank_logger(data[0], rank_num, "Increase")
+                    alter_ranks(data[0], rank_num, format_date, 1)
+
+                elif rank_num == current_rank:
+                    alter_ranks(data[0], '', format_date, 2)
 
     except IndexError:
-        if rank_num >= 0 and rank_num <= 6:
-            c.execute("INSERT OR IGNORE INTO member ({}) VALUES('{}', '{}', '{}')".format(
-                "name, rank_id, last_seen", name, rank_num + 1, format_time))
-            conn.commit()
-            logging.info('ACTION: {} | NAME: {} | STATUS: {}'.format(
-                'Append', name, ranks[rank_num]))
+        if 0 <= rank_num <= 6:
+            rank_logger(name, rank_num, "Append")
+            alter_ranks(name, rank_num, format_date, 3)
         else:
             pass
 
-rank_tracker(arg_name, arg_rank)
+
+rank_tracker(str(argv[1]), int(argv[2]))
