782e46f1fd202ee9411b8c53dfe52a211d9d9f49
utsukami <geremysowers@live.com>
2018-07-25 23:44:15 -0400

    keep & restore notes after inactive

--- botkue/data_tracker.py
+++ botkue/data_tracker.py
@@ -1,87 +1,66 @@
-import logging
+import sql_cmds
 from datetime import datetime
 from os.path import expanduser
+from rank_logger import rank_logger
 from sqlite3 import connect
 from sys import argv
 
 home = expanduser('~')
-log_file = '{}/tmp/main.log'.format(home)
 format_date = datetime.utcnow().strftime("%Y-%m-%d")
 
-conn = connect('{}/tmp/main.sqlite'.format(home))
-c = conn.cursor()
-
-logging.basicConfig(
-    filename=log_file,
-    level=logging.INFO,
-    format='{}'.format('[%(asctime)s] %(message)s'),
-    datefmt='{}'.format('%Y-%m-%d %H:%M:%S')
-)
+conn_main = connect('{}/tmp/main.sqlite'.format(home))
+conn_lost = connect("{}/tmp/lost.sqlite".format(home))
+cdb = conn_main.cursor()
 
 
-def rank_logger(name, rank, action):
-    ranks = (
-        "Smiley", "Recruit", "Corporal", "Sergeant",
-        "Lieutenant", "Captain", "General"
-    )
+def check_exists(name, database):
+    sql_cmds.select_membs(name, database)
 
-    logging.info(
-        "ACTION: {} | NAME: {} | STATUS: {}"
-        .format(action, name, ranks[rank])
-    )
+    try:
+        return database.fetchall()[0]
 
-
-def alter_ranks(name, rank, date, method):
-    if method == 1:
-        c.execute(
-            "UPDATE member SET "
-            "rank_id=?, last_seen=?, date_rank=? WHERE name=?",
-            (rank + 1, date, date, name,)
-        )
-
-    elif method == 2:
-        c.execute(
-            "UPDATE member SET last_seen=? WHERE name=?", (date, name,)
-        )
-
-    elif method == 3:
-        c.execute(
-            "INSERT OR IGNORE INTO member (name, rank_id, last_seen) "
-            "VALUES(?,?,?)", (name, rank + 1, date,)
-        )
-
-    conn.commit()
+    except IndexError:
+        return False
 
 
 def rank_tracker(name, rank_num):
-    c.execute(
-        "SELECT name, rank_id FROM member WHERE name=?", (name,)
-    )
+    data = check_exists(name, cdb)
 
-    try:
-        data = c.fetchall()[0]
+    if data:
+        current_rank = int(data[1]) - 1
 
-        if name in data[0]:
-            current_rank = int(data[1]) - 1
-
-            if 0 <= rank_num <= 6:
-                if rank_num < current_rank:
-                    rank_logger(data[0], rank_num, "Decrease")
-                    alter_ranks(data[0], rank_num, format_date, 1)
-
-                elif rank_num > current_rank:
-                    rank_logger(data[0], rank_num, "Increase")
-                    alter_ranks(data[0], rank_num, format_date, 1)
-
-                elif rank_num == current_rank:
-                    alter_ranks(data[0], '', format_date, 2)
-
-    except IndexError:
         if 0 <= rank_num <= 6:
-            rank_logger(name, rank_num, "Append")
-            alter_ranks(name, rank_num, format_date, 3)
-        else:
-            pass
+            if rank_num < current_rank:
+                rank_logger(data[0], rank_num, "Decrease")
+                sql_cmds.change_curr(data[0], rank_num, format_date, cdb)
+
+            elif rank_num > current_rank:
+                rank_logger(data[0], rank_num, "Increase")
+                sql_cmds.change_curr(data[0], rank_num, format_date, cdb)
+
+            elif rank_num == current_rank:
+                sql_cmds.keep_curr(data[0], format_date, cdb)
+
+    elif not data:
+        ldb = conn_lost.cursor()
+        check_lost = check_exists(name, ldb)
+
+        if not check_lost:
+            if 0 <= rank_num <= 6:
+                rank_logger(name, rank_num, "Append")
+                sql_cmds.append_new(name, rank_num, format_date, cdb)
+
+        elif check_lost:
+            if 0 <= rank_num <= 6:
+                rank_logger(name, rank_num, "Restore")
+                sql_cmds.append_old(
+                    name, rank_num, format_date, check_lost[3], cdb
+                )
+
+                sql_cmds.delete_rank(name, ldb)
+                conn_lost.commit()
+
+    conn_main.commit()
 
 
 rank_tracker(str(argv[1]), int(argv[2]))
