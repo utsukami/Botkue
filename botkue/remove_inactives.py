c04082f3edf3571b76ad7417a6ac4e3acaa7b665
utsukami <geremysowers@live.com>
2018-08-31 17:29:42 -0400

    Updated to use verify_date from sql_cmds

--- botkue/remove_inactives.py
+++ botkue/remove_inactives.py
@@ -4,36 +4,29 @@ from os.path import expanduser
 from rank_logger import rank_logger
 from sqlite3 import connect
 
-home = expanduser('~')
+home = expanduser("~")
 curr_utc = datetime.utcnow()
 
-conn_main = connect('{}/tmp/main.sqlite'.format(home))
-conn_lost = connect('{}/tmp/lost.sqlite'.format(home))
+conn_main = connect("{}/databases/main.sqlite".format(home))
+conn_lost = connect("{}/databases/lost.sqlite".format(home))
 cdb = conn_main.cursor()
 
 
 def remove_ranks(limiter, rank):
     cdb.execute("SELECT name, rank_id, last_seen, notes FROM member")
+    data = cdb.fetchall()
 
-    for data in cdb.fetchall():
-        find_year = data[2].index("-")
-        find_day = data[2].rindex("-")
-
-        get_year = data[2][:find_year]
-        get_day = data[2][int(find_day + 1):]
-        get_month = data[2][int(find_year + 1):find_day]
-
-        full_afk = (curr_utc - datetime(
-            int(get_year), int(get_month), int(get_day))
-        ).days
+    for names in data:
+        full_afk = sql_cmds.verify_date(names)
 
         if (full_afk >= limiter
-                and data[1] <= rank):
+                and names[1] <= rank):
+
             ldb = conn_lost.cursor()
 
-            rank_logger(data[0], data[1], "Inactive")
-            sql_cmds.restore_old(data[0], data[1], data[2], data[3], ldb)
-            sql_cmds.delete_rank(data[0], cdb)
+            rank_logger(names[0], names[1], "Inactive")
+            sql_cmds.restore_old(names[0], names[1], names[2], names[3], ldb)
+            sql_cmds.delete_rank(names[0], cdb)
 
             conn_lost.commit()
             conn_main.commit()
