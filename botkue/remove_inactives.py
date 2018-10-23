8b5b2be596e465565515f95bea39d968e245407e
utsukami <geremysowers@live.com>
2018-10-22 23:25:38 -0400

    Updated to match values changed in sql_cmds.py

--- botkue/remove_inactives.py
+++ botkue/remove_inactives.py
@@ -7,26 +7,29 @@ from sqlite3 import connect
 home = expanduser("~")
 curr_utc = datetime.utcnow()
 
-conn_main = connect("{}/databases/main.sqlite".format(home))
-conn_lost = connect("{}/databases/lost.sqlite".format(home))
+conn_main = connect("{}/main.sqlite".format(home))
+conn_lost = connect("{}/lost.sqlite".format(home))
 cdb = conn_main.cursor()
 
 
 def remove_ranks(limiter, rank):
-    cdb.execute("SELECT name, rank_id, last_seen, notes FROM member")
+    cdb.execute("SELECT * FROM member")
     data = cdb.fetchall()
 
     for names in data:
         full_afk = sql_cmds.verify_date(names)
 
         if (full_afk >= limiter
-                and names[1] <= rank):
+                and names[2] <= rank):
 
             ldb = conn_lost.cursor()
 
-            rank_logger(names[0], names[1], "Inactive")
-            sql_cmds.restore_old(names[0], names[1], names[2], names[3], ldb)
-            sql_cmds.delete_rank(names[0], cdb)
+            rank_logger(names[1], names[2], "Inactive")
+            sql_cmds.restore_old(
+                    names[1], names[2], names[3], names[4],
+                    names[5], names[6], names[7], ldb
+            )
+            sql_cmds.delete_rank(names[1], cdb)
 
             conn_lost.commit()
             conn_main.commit()
